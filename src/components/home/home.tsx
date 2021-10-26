8dfa1bd9e5d4d15010388d127336bf74c1cfbd99
Lucas Leguizamo <78978776+Lucas-FullStackX@users.noreply.github.com>
2021-10-26 10:33:59 -0500

    test

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -8,6 +8,7 @@ export default function Home(){
     const {loading,error,data}= useQuery(query.GET_INFO)
     console.log(data)
     if(error){
+        console.log('test')
         console.log(error)
     }
     return(<div>{loading ? (<div>loading...</div>) : error? (<div>error{error}</div>): (<div><h1>{data.company.name}</h1></div>)}</div>)
